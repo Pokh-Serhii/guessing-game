fec085fd69027ef7ac15e6ec8f7a1204dd664da2
Serhii Pokh <117908961+Pokh-Serhii@users.noreply.github.com>
2023-02-26 19:18:15 +0200

    feat: update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max)/2);
+  
+        return this.result;
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max)/2);
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max)/2);
     }
 }
-
 module.exports = GuessingGame;
